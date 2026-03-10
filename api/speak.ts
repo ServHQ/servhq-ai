a09cac52e5eac5f0e3d6015e9fbe729694613741
ServHQ <info@servhq.com.au>
2026-03-10 11:44:37 +1000

    Update speak.ts

--- api/speak.ts
+++ api/speak.ts
@@ -5,8 +5,8 @@ const client = new OpenAI({
 });
 
 function setCors(res: any) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "https://servhq.com.au");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 }
 
@@ -15,6 +15,10 @@ export default async function handler(req: any, res: any) {
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
+  }
+
+  if (req.method === "GET") {
+    return res.status(200).json({ status: "ok", message: "Speech API is live" });
   }
 
   if (req.method !== "POST") {
